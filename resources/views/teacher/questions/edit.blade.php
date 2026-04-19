273b8d7811376aeb8d631f40cfb82ddd0eca5abc
raykkun <raiwijaya2016@gmail.com>
2026-04-19 09:04:56 +0700

    update scripts

--- resources/views/teacher/questions/edit.blade.php
+++ resources/views/teacher/questions/edit.blade.php
@@ -76,7 +76,7 @@
 
                 <div class="mt-3">
                     <button type="submit" class="inline-block px-6 py-2.5 bg-blue-600 text-white rounded-full hover:bg-blue-700">Update Question</button>
-                    <a href="{{ route('teacher.my-exams.show', $question->exam_id) }}" class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 rounded-full hover:bg-gray-300">Cancel</a>
+                    <a href="{{ route('teacher.exams.show', $question->exam_id) }}" class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 rounded-full hover:bg-gray-300">Cancel</a>
                 </div>
             </form>
         </div>
