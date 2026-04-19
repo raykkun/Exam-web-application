3dadeb3543b889fc3bbb02d21223960eef40ccb2
raykkun <raiwijaya2016@gmail.com>
2026-04-19 12:04:03 +0700

    update script for classroom code before readonly to required

--- resources/views/classrooms/edit.blade.php
+++ resources/views/classrooms/edit.blade.php
@@ -74,7 +74,7 @@
                     ease-in-out
                     m-0
                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                  " name="code" value="{{ old('code', $classroom->code) }}" readonly>
+                  " name="code" value="{{ old('code', $classroom->code) }}" required>
 
                 <!-- error message untuk code -->
                 @error('code')
